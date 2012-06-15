8904ca81e3295e8f7933ee61d40e0d5e0e50071e
Oguz Bilgic <fisyonet@gmail.com>
2012-06-15 07:45:30 +0300

    Add ruby-source resource

---
+++ resrouce/ruby-source.sh
@@ -0,0 +1,15 @@
+# Compile and Install ruby from source
+apt-get -y install build-essential
+
+# Download ruby
+cd /tmp
+wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz
+tar -xvzf ruby-1.9.3-p194.tar.gz
+cd ruby-1.9.3-p194/
+
+# Compile ruby
+./configure
+make
+
+# Install ruby
+sudo make install
