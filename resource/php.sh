5c034065eaa27aa2d7c50c5608db2e5d61cfd2fb
Oguz Bilgic <fisyonet@gmail.com>
2012-07-07 03:37:50 -0700

    Add php resource

---
+++ resource/php.sh
@@ -0,0 +1,2 @@
+# Install php5
+apt-get install -y php5
--- stack/php-apache.sh
+++ stack/php-apache.sh
@@ -1,9 +1,10 @@
 # Require
 bash resource/apache.sh
 bash resource/mysql-client.sh
+bash resource/php.sh
 
-# Install php5 and modules
-apt-get install --yes php5 php5-mysql libapache2-mod-php5
+# Install php5 modules and bindings
+apt-get install --yes php5-mysql libapache2-mod-php5
 service apache2 reload
 
 # Create /info.php file
