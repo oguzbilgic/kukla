e9c2a7520092833486e00e0ba7e11e217378ee0f
Oguz Bilgic <fisyonet@gmail.com>
2012-11-29 18:29:10 -0800

    Improve bashrc resource

--- resource/bashrc.sh
+++ resource/bashrc.sh
@@ -1,2 +1,8 @@
 # Uncomment line in bashrc, so it forces prompt to be colorful
-sed -e 's/#force_color_prompt=yes/force_color_prompt=yes/' /root/.bashrc
+if [ -f /root/.bashrc ]; then
+    sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' /root/.bashrc
+fi
+
+if [ -f /home/ubuntu/.bashrc ]; then
+    sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' /home/ubuntu/.bashrc
+fi
